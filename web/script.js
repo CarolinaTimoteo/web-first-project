a26387c55c2bd5cf29cd4f36f39a39e6c533100d
unknown <carol.timot@gmail.com>
2024-01-28 21:11:50 -0300

    soma

--- web/script.js
+++ web/script.js
@@ -1,1 +1,10 @@
-alert("ola");
+
+
+function calcular() {
+    var valor1 = Number(document.getElementById("valor1").value);
+    var valor2 = Number(document.getElementById("valor2").value);
+    document.getElementById("resultado").value= valor1 + valor2;
+    
+
+}
+    
